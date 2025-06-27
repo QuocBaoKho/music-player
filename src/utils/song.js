d3af279506e8ff6023e82fea594e53b3f749eff2
QuocBaoKho <baonhq2603@gmail.com>
2025-06-27 21:04:30 +0700

    Add new songs

--- src/utils/song.js
+++ src/utils/song.js
@@ -23,11 +23,20 @@ const songs = [
   },
   {
     id: 4,
-    title: "Take This Bottle",
-    artist: "Faith No More",
-    src: "/audio/Take_This_Bottle.mp3",
-    cover: "/image/Faith_No_More_-_King_for_a_Day..._Fool_for_a_Lifetime.png",
+    title:
+      "Eyes Without A Face - February 09 2024 - Live in The Venue at Thunder Valley",
+    artist: "Billy Idol",
+    src: "/audio/eyes_without_a_face_2024.mp3",
+    cover: "/image/Eyes_Without_A_Face_Cover.jpg",
   },
+  {
+    id: 5,
+    title: "Faith No More - Ashes To Ashes & Just A Man (Phoenix '97)",
+    artist: "Faith No More",
+    src: "/audio/ashes_to_ashes_just_a_man.mp3",
+    cover: "/image/ashes_to_ashes_just_a_man.png",
+  },
+
   // Thêm các bài hát khác vào đây
 ];
 
